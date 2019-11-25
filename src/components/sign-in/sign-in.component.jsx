de4085acc608ab6f20ad82755df679e5e9d71045
Greg Jackson <gregoryljackson@gmail.com>
2019-11-25 11:31:26 -0500

    finishing sign in handleSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -16,10 +16,18 @@ class SignIn extends React.Component {
     }
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
+    const { email, password } = this.state;
 
-    this.setState({ email: '', password: '' })
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' })
+
+    } catch (error) {
+      console.error(error);
+    }
+
   }
 
   handleChange = event => {
